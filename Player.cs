83041ecc5098af2fd93c7479e16a90c6156522c8
Drennon Kingore <dk1008065@otc.edu>
2025-01-16 08:59:54 -0600

    first commit

---
+++ Player.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Player
+{
+   private string initials { get; set; }
+   private int score { get; set; }
+   
+   public Player (string Initials, int Score)
+   {
+        initials = Initials;
+        score = Score;
+   }
+   public void DiaplayPlayer(int programPlayers)
+   {
+        Console.WriteLine($"Player: {initials}, {score}");
+   }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main ()
+    {
+        Player players1 = new Player("DK", 15);
+        Player players2 = new Player("MG", 20);
+        players1.DiaplayPlayer(1);
+        players2.DiaplayPlayer(2);
+    }
+}
